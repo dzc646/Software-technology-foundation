48453d7638354d4a15b1eedb6db541c31ae94ea8
dzc646 <18757591962@163.com>
2019-10-21 23:37:53 +0800

    v0.1 空项目

--- WordCount/wordcount.cpp
+++ WordCount/wordcount.cpp
@@ -6,78 +6,25 @@ using namespace std;
 
 void main()
 {
-	char  c, x;
-	int i = 0, inword = 0,ture=1;
-	string filename;
-	int line = 0;
-	int word = 0;
-	int character = 0;
-	int sentence = 0;
 	
-	while (ture)
+	string filename;
+	
+	
+	while (1)
 	{
 		
-		cin >> x;
-		if (x != 'w' & x != 'c' & x != 's' & x!='l')
-			{
-				cout << "输入出错！！！\n请从新输入...\n" << endl;
-				continue;
-			}
-		cin >> filename;
 		
-		cout << "开始读取文件..." << endl;
 		ifstream ifile;
-		ifile.open(filename, ios::in);
+		ifile.open("wc.txt", ios::in);
 		if (!ifile)
 			{
 				cerr << "Open File Fail.\n请重新输入..." << endl;
-				continue;
+				break;
 			}
 		cout << "读取成功！\n" << endl;
 
-		switch (x)
-		{	case 'l':
-				while (ifile.get(c))
-				{
-					if (c == '\n')
-						line++;
-				} 
-				cout << "line is:" << line << endl;
-				break;
-			case 'w':
-				while (ifile.get(c))
-				{
-					if (!isspace(c) && !inword)     //isspace (c)检查参数c是否为非字母的字符，如：('')、('\t')、('\r')、('\n')、('\v')、('\f')
-					{
-						inword = 1;
-						word++;
-					}
-					if (isspace(c) && inword)
-						inword = 0;				
-				}
-				cout << "word is:" << word << endl;
-				break;
-			case 'c':
-				while (ifile.get(c))
-				{
-					//if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-						character++;
-				}
-				cout << "character is:" << character << endl;
-				break;
-			case 's':
-				while (ifile.get(c))
-				{
-					if ( c == '.'|| c== '?' )
-						sentence++;
-				}
-				cout << "sentence is:" << sentence << endl;
-				break;
-
-		}
-
 		ifile.close();
-		ture = 0;
+		
 		
 	}
 	system("pause");
